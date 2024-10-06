13b040a24beff01be51e181b75667b04c0525283
kristian <krestrepoo@eafit.edu.co>
2024-10-06 13:14:55 -0500

    feat: add input guidelines to help user to use the method

--- applications/numerical_method/urls.py
+++ applications/numerical_method/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views
+from . import views as numerical_method
 
 app_name = "numerical_method_app"
-urlpatterns = []
+urlpatterns = [
+    path("bisection/", numerical_method.bisection.as_view(), name="numerical_method.bisection"),
+]
