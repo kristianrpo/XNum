c909f3aca54f9062ce408edb6788a3377570a230
kristian <krestrepoo@eafit.edu.co>
2024-10-09 15:23:57 -0500

    feat: register regula falsi service in containers

--- src/application/numerical_method/containers/numerical_method_container.py
+++ src/application/numerical_method/containers/numerical_method_container.py
@@ -1,6 +1,10 @@
 from dependency_injector import containers, providers
 from src.application.numerical_method.services.bisection_service import BisectionService
+from src.application.numerical_method.services.regula_falsi_service import (
+    RegulaFalsiService,
+)
 
 
 class NumericalMethodContainer(containers.DeclarativeContainer):
     bisection_service = providers.Factory(BisectionService)
+    regula_falsi_service = providers.Factory(RegulaFalsiService)
