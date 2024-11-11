5a9d8d9fceed1d2fe15f62c21291baa2202b6145
kristian <krestrepoo@eafit.edu.co>
2024-11-10 20:46:52 -0500

    feat: add util function to build polynomials

--- src/application/shared/utils/plot_function.py
+++ src/application/shared/utils/plot_function.py
@@ -2,49 +2,57 @@ import numpy as np
 from config.settings import BASE_DIR
 import matplotlib.pyplot as plt
 import math
-
-"""
-Función que se encarga de generar un archivo vectorizado de la gráfica de una función matemática.
-Un archivo vectorizado es un tipo de archivo gráfico que almacena imágenes usando vectores en lugar de píxeles. Los gráficos vectorizados se definen mediante formas geométricas como líneas, curvas, círculos, y polígonos, descritos matemáticamente a través de fórmulas.
-"""
+import textwrap
 
 
-def plot_function(function_f: str, have_solution: bool, root: float) -> None:
+def plot_function(
+    function_f: str, have_solution: bool, points: list[tuple[float, float]]
+) -> None:
     output_file = BASE_DIR / "static/img/numerical_method/function_plot.svg"
 
-    # Se crea una figura de tamaño 6x4 pulgadas, es como el lienzo sobre el cual se pinta el gráfico.
-    plt.figure(figsize=(6, 4))
+    # Obtener los valores de x y y de los puntos para el rango de la gráfica
+    x_coords = [point[0] for point in points]
+    y_coords = [point[1] for point in points]
 
-    # Generamos un rango de valores para 'x'. Esto quiero decir que vamos a evaluar la función en un rango de valores de 'x' para poder graficarla.
-    # Este rango de valores se permite generar utilizando np.linspace donde permite obtener un array de valores de un intervalo, de manera equiespaciada según la cantidad de valores que se necesiten para ese intervalo.
+    min_x, max_x = min(x_coords) - 1, max(x_coords) + 1
+    min_y, max_y = min(y_coords) - 1, max(y_coords) + 1
+
+    # Generar un rango más ajustado de valores de x para la evaluación de la función
     if not have_solution:
         x_vals = np.linspace(-100, 100, 1000)
     else:
-        x_vals = np.linspace(root - 3, root + 3, 1000)
-        # Se grafican los puntos de intersección con y = 0, para las raices que son numeros reales y que se encuentran en el intervalo.
-        plt.plot(root, 0, marker="o", color="#f7dc6f")
-        plt.text(root, 0, f"({root}, 0)", fontsize=9, verticalalignment="bottom")
+        x_vals = np.linspace(min_x - 3, max_x + 3, 1000)
 
-    # Evaluamos la función en el rango de valores de 'x' que generamos anteriormentey almacenamos cada resultado en una lista.
     y_vals = [eval(function_f, {"math": math, "x": val}) for val in x_vals]
 
+    # Crear la figura
+    plt.figure(figsize=(6, 4))
+
+    # Graficar la función en el rango ajustado
+    plt.plot(x_vals, y_vals, color="#a18262", label="f(x)")
+
+    # Graficar cada punto de intersección proporcionado
+    for x, y in points:
+        plt.plot(x, y, marker="o", color="#f7dc6f")
+        plt.text(x, y, f"({x}, {y})", fontsize=9, verticalalignment="bottom")
+
     # Ajustar los límites de los ejes para el recuadro de la grafica como tal
-    plt.xlim(root - 5, root + 5)  # Ajusta el rango horizontal para una vista más amplia
-    plt.ylim(-4, 4)  # Ajusta el rango vertical para una vista más centrada
+    plt.xlim(min_x - 3, max_x + 3)
+    plt.ylim(min_y - 3, max_y + 3)
 
-    # Se grafica la función con el conjunto de x's y y's que generamos anteriormente en el rango especificado.
-    plt.plot(x_vals, y_vals, color="#a18262")
-    # Se establece una linea punteada roja en y=0 para identificar la raíz.
+    # Ejes y etiquetas
     plt.axhline(y=0, color="red", linestyle="--", linewidth=1)
-    # Se establece una etiqueta para identificar eje 'x'.
+    plt.axvline(x=0, color="red", linestyle="--", linewidth=1)
     plt.xlabel("x")
-    # Se establece una etiqueta para identificar eje 'y'.
     plt.ylabel("y")
-    # Se establece el titulo de la grafica en la parte superior de la misma.
-    plt.title(f"y = {function_f}")
-    # Se establece que aparezca una cuadricula en el gráfico donde va la función.
-    plt.grid(True)
 
-    # Guardamos la gráfica en un archivo SVG
+    wrapped_title = "\n".join(textwrap.wrap(function_f, width=50))
+    plt.title(f"f(x) = {wrapped_title}", fontsize=10, y=1.1)
+
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+
+    # Guardar la gráfica
     plt.savefig(output_file, format="svg")
     plt.close()
